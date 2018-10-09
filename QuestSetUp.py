43f3f69f4d78c4e6c9a6847ec3a8bcb957da23cc
Maddie Wagner <musomaddy@gmail.com>
2018-10-09 22:21:45 +1100

    Added Cooks Assistant

--- QuestSetUp.py
+++ QuestSetUp.py
@@ -7,6 +7,7 @@ sys.path.insert(0,
 from quests.Beneath_Cursed_Tides import Beneath_Cursed_Tides
 from quests.The_Blood_Pact import The_Blood_Pact
 from quests.Broken_Home import Broken_Home
+from quests.Cooks_Assistant import Cooks_Assistant
 
 
 def create_all_quests():
@@ -14,3 +15,13 @@ def create_all_quests():
     beneath_cursed_tides = Beneath_Cursed_Tides()
     the_blood_pact = The_Blood_Pact()
     broken_home = Broken_Home()
+    cooks_assistant = Cooks_Assistant()
+
+    print(beneath_cursed_tides,
+          the_blood_pact,
+          broken_home,
+          cooks_assistant,
+          "done")
+
+
+create_all_quests()

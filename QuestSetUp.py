87415d2463e80ef409675ed60e40e0a20219fd79
Maddie Wagner <musomaddy@gmail.com>
2018-10-09 22:25:14 +1100

    The Death of Chivalry quest added

--- QuestSetUp.py
+++ QuestSetUp.py
@@ -8,6 +8,7 @@ from quests.Beneath_Cursed_Tides import Beneath_Cursed_Tides
 from quests.The_Blood_Pact import The_Blood_Pact
 from quests.Broken_Home import Broken_Home
 from quests.Cooks_Assistant import Cooks_Assistant
+from quests.The_Death_Of_Chivalry import The_Death_Of_Chivalry
 
 
 def create_all_quests():
@@ -16,11 +17,13 @@ def create_all_quests():
     the_blood_pact = The_Blood_Pact()
     broken_home = Broken_Home()
     cooks_assistant = Cooks_Assistant()
+    the_death_of_chivalry = The_Death_Of_Chivalry()
 
     print(beneath_cursed_tides,
           the_blood_pact,
           broken_home,
           cooks_assistant,
+          the_death_of_chivalry,
           "done")
 
 
---
+++ quests/The_Death_Of_Chivalry.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+sys.path.insert(0,
+                os.path.dirname(os.path.realpath(__file__))[
+                    0:-len("quests")])
+from QuestInfo import Quest
+
+
+class The_Death_Of_Chivalry(Quest):
+
+    def __init__(self):
+        super().__init__("The Death of Chivalry")
+        self.free = True

792fa672c4f018bada1efaae1bd01bb56b356a0b
Maddie Wagner <musomaddy@gmail.com>
2018-10-09 22:14:33 +1100

    The blood pact added

--- QuestSetUp.py
+++ QuestSetUp.py
@@ -5,8 +5,10 @@ sys.path.insert(0,
                 os.path.dirname(os.path.realpath(__file__))[
                     0:-len("")])
 from quests.Beneath_Cursed_Tides import Beneath_Cursed_Tides
+from quests.The_Blood_Pact import The_Blood_Pact
 
 
 def create_all_quests():
 
     beneath_cursed_tides = Beneath_Cursed_Tides()
+    the_blood_pact = The_Blood_Pact()

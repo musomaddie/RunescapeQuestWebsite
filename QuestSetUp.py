67658f8cb339255239d62b412f712f915988abdd
Maddie Wagner <musomaddy@gmail.com>
2018-10-13 22:41:38 +1100

    added All Fired Up quest'

--- QuestSetUp.py
+++ QuestSetUp.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(0,
                 os.path.dirname(os.path.realpath(__file__))[
                     0:-len("")])
+from quests.All_Fired_Up import All_Fired_Up
 from quests.Beneath_Cursed_Tides import Beneath_Cursed_Tides
 from quests.The_Blood_Pact import The_Blood_Pact
 from quests.Broken_Home import Broken_Home
@@ -42,6 +43,8 @@ from quests.Wolf_Whistle import Wolf_Whistle
 
 
 def create_all_quests():
+
+    all_fired_up = All_Fired_Up()
 
     beneath_cursed_tides = Beneath_Cursed_Tides()
     the_blood_pact = The_Blood_Pact()
@@ -127,6 +130,7 @@ def create_all_quests():
           whats_mine_is_yours,
           witchs_house,
           wolf_whistle,
+          all_fired_up,
           "done")
 
 

e68d0c19ef5d2b4d16ddacf69b7edcf207280cbf
Maddie Wagner <musomaddy@gmail.com>
2018-10-14 18:22:11 +1100

    Added Buyers and Cellars Quest

--- QuestSetUp.py
+++ QuestSetUp.py
@@ -21,6 +21,7 @@ from quests.The_Branches_Of_Darkmeyer import The_Branches_Of_Darkmeyer
 from quests.Bringing_Home_The_Bacon import Bringing_Home_The_Bacon
 from quests.The_Brink_Of_Extinction import The_Brink_Of_Extinction
 from quests.Broken_Home import Broken_Home
+from quests.Buyers_And_Cellars import Buyers_And_Cellars
 from quests.Cooks_Assistant import Cooks_Assistant
 from quests.The_Death_Of_Chivalry import The_Death_Of_Chivalry
 from quests.Death_Plateau import Death_Plateau
@@ -75,6 +76,7 @@ def create_all_quests():
     bringing_home_the_bacon = Bringing_Home_The_Bacon()
     the_brink_of_extinction = The_Brink_Of_Extinction()
     broken_home = Broken_Home()
+    buyers_and_cellars = Buyers_And_Cellars()
 
     cooks_assistant = Cooks_Assistant()
 

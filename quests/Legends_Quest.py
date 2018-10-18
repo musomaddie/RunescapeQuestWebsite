1086c9b35c499dcb9798879968bd54a383a0f358
Maddie Wagner <musomaddy@gmail.com>
2018-10-18 16:30:34 +1100

    Added Legends Quest

---
+++ quests/Legends_Quest.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.insert(0,
+                os.path.dirname(os.path.realpath(__file__))[
+                    0:-len("quests")])
+from QuestInfo import Quest
+
+
+class Legends_Quest(Quest):
+
+    def __init__(self):
+        super().__init__("Legends' Quest")
+        self.age = 5
+        self.difficulty = "Master"
+        self.length = "Long to Very Long"
+        self.quest_points = 4
+
+        self.agility = 50
+        self.crafting = 50
+        self.herblore = 45
+        self.magic = 56
+        self.mining = 52
+        self.prayer = 42
+        self.smithing = 50
+        self.strength = 50
+        self.thieving = 50
+        self.woodcutting = 50

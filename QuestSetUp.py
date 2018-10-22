3dabd1971d7f636d160fcbef81e9c1965e571a74
Maddie Wagner <musomaddy@gmail.com>
2018-10-22 16:54:59 +1100

    Added Murder Mystery quest

--- QuestSetUp.py
+++ QuestSetUp.py
@@ -136,6 +136,7 @@ from quests.Monks_Friend import Monks_Friend
 from quests.Mountain_Daughter import Mountain_Daughter
 from quests.Mournings_End_Part_I import Mournings_End_Part_I
 from quests.Mournings_End_Part_II import Mournings_End_Part_II
+from quests.Murder_Mystery import Murder_Mystery
 from quests.Myths_Of_The_White_Lands import Myths_Of_The_White_Lands
 from quests.One_Piercing_Note import One_Piercing_Note
 from quests.Perils_Of_Ice_Mountain import Perils_Of_Ice_Mountain
@@ -301,6 +302,7 @@ def create_all_quests():
     mountain_daughter = Mountain_Daughter()
     mournings_end_part_i = Mournings_End_Part_I()
     mournings_end_part_ii = Mournings_End_Part_II()
+    murder_mystery = Murder_Mystery()
     myths_of_the_white_lands = Myths_Of_The_White_Lands()
 
     one_piercing_note = One_Piercing_Note()
@@ -474,6 +476,7 @@ def create_all_quests():
                   mountain_daughter,
                   mournings_end_part_i,
                   mournings_end_part_ii,
+                  murder_mystery,
                   "done"]
     print(all_quests)
 

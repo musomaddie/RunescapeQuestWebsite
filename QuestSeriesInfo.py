fb05a8404bbaf16f484d668239c5761ef2cdfdba
Maddie Wagner <musomaddy@gmail.com>
2018-11-05 00:25:05 +1100

    Added a quest ordering system to the serie: s

--- QuestSeriesInfo.py
+++ QuestSeriesInfo.py
@@ -5,8 +5,8 @@ class QuestSeries:
         self.quests = []
         self.related_quests = []
 
-    def add_quest(self, quest):
-        self.quests.append(quest)
+    def add_quest(self, quest, n):
+        self.quests.append((quest, n))
 
     def add_related_quest(self, quest):
         self.related_quests.append(quest)
